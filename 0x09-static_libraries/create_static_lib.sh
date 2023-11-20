4f91c26df0d87ed0c30241a63d682140ead1830a
unknown <kidus.muluwork@gmail.com>
2023-11-20 14:50:17 +0300

    modified create_static_lib.sh file

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
 gcc -c *.c
  ar -rc liball.a *.o
+
+#!/bin/bash
+
+# Compile all .c files in the current directory into object files
+gcc -c *.c
+
+# Create the static library liball.a from all object files
+ar rc liball.a *.o
+
+# Index the static library
+ranlib liball.a
+
+# Clean up: Remove the temporary object files
+rm -f *.o
